aefe0269ac42f2311f355d63759c433f09945306
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-10-12 16:16:40 -0300

    fix chats tests

--- src/test/java/com/fatec/chatapp/ChatsTests.java
+++ src/test/java/com/fatec/chatapp/ChatsTests.java
@@ -1,15 +1,18 @@
 package com.fatec.chatapp;
 
 import com.fatec.chatapp.chats.ChatModel;
+import com.fatec.chatapp.chats.ChatsController;
 import com.fatec.chatapp.chats.ChatsRepository;
 import com.fatec.chatapp.chats.ChatsServiceImpl;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +21,14 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ChatsTests {
+class ChatsServiceTests {
     @MockBean
     private ChatsRepository chatsRepository;
 
@@ -31,31 +38,45 @@ public class ChatsTests {
     final List<ChatModel> chats = new ArrayList<>();
     final ChatModel chatOne = new ChatModel(UUID.randomUUID(),"String",true);
 
-    @Ignore
     @Test
     public void contextLoads() throws Exception {
-        chats.add(chatOne);
-
-        given(chatsRepository.findAll()).willReturn(chats);
-        given(chatsRepository.save(chatOne)).willReturn(chatOne);
-
         assertThat(chatService).isNotNull();
     }
 
-    @Ignore
     @Test
     public void shouldCreateChat() throws  Exception{
-//        ChatModel stub = chatService.create(chatOne);
-//
-//        assertEquals(chatOne.getId(), stub.getId());
-//        assertEquals(chatOne.getIsActive(), stub.getIsActive());
+        given(chatsRepository.save(chatOne)).willReturn(chatOne);
+        ChatModel stub = chatService.create(chatOne);
+
+        assertEquals(chatOne.getId(), stub.getId());
+        assertEquals(chatOne.getIsActive(), stub.getIsActive());
     }
 
     @Ignore
     @Test
-    public void shouldGetAllChats() throws Exception{
-//        List<ChatModel> stub = chatService.getAll();
-//
-//        assertEquals(1, stub.size());
+    public void shouldGetAllChats() throws Exception {
+        chats.add(chatOne);
+        given(chatsRepository.findAll()).willReturn(chats);
+
+        List<ChatModel> stub = chatService.getAll();
+
+        assertEquals(1, stub.size());
     }
 }
+
+@WebMvcTest(ChatsController.class)
+class ChatsControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ChatsServiceImpl chatsService;
+
+    @Test
+    public void shouldFetchAll() throws Exception {
+        List<ChatModel> chats = new ArrayList<>();
+        when(chatsService.getAll()).thenReturn(chats);
+        this.mockMvc.perform(get("/chats")).andDo(print()).andExpect(status().isOk());
+    }
+}
+
