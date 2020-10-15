d4406327614a46e766c30b0d55ac312bc3107175
Paulo Keller <paulo.vinicius.keller@gmail.com>
2020-10-14 23:06:03 -0300

    cors config (#23)

--- src/main/java/com/fatec/chatapp/ChatAppApplication.java
+++ src/main/java/com/fatec/chatapp/ChatAppApplication.java
@@ -2,6 +2,9 @@ package com.fatec.chatapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChatAppApplication {
@@ -9,5 +12,4 @@ public class ChatAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatAppApplication.class, args);
 	}
-
 }
