dbf805583bb24674f32ac91a7ab4c7a4f6eeb8a5
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-10-25 17:38:04 -0300

    security service, security base routes config done

--- src/main/java/com/fatec/chatapp/config/SecurityConfig.java
+++ src/main/java/com/fatec/chatapp/config/SecurityConfig.java
@@ -1,20 +1,27 @@
 package com.fatec.chatapp.config;
 
-import com.fatec.chatapp.userdetails.MyUserDetailsService;
+import com.fatec.chatapp.authentication.MyUserDetailsService;
+import com.fatec.chatapp.authentication.AuthenticationRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private MyUserDetailsService myUserDetailsService;
+
+  @Autowired
+  private AuthenticationRequestFilter authenticationRequestFilter;
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,7 +30,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests().antMatchers("/authentication", "/explorer").permitAll().anyRequest().authenticated();
+    http.csrf().disable().authorizeRequests()
+            .antMatchers("/authentication", "/explorer").permitAll()
+            .antMatchers(HttpMethod.POST, "/users").permitAll()
+            .anyRequest().authenticated()
+            .and().sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.addFilterBefore(authenticationRequestFilter, UsernamePasswordAuthenticationFilter.class);
   }
 
   @Override
