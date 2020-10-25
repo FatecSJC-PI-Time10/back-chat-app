6f7966b6d128e99f4cf4d8b8a96763b5eadfe96c
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-10-25 13:05:30 -0300

    jwt support added

---
+++ src/main/java/com/fatec/chatapp/authentication/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.fatec.chatapp.authentication;
+
+public class AuthenticationResponse {
+  private final String jwt;
+
+  public AuthenticationResponse(String jwt) {
+    this.jwt = jwt;
+  }
+
+  public String getJwt() {
+    return jwt;
+  }
+}
